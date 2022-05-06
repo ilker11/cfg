048e92da2cbb9fb7f6c75b15f50ae80b2865611d
ilker11 <ilker01@abv.bg>
2022-05-06 18:08:54 +0300

    FileReader and FileWriter added.

---
+++ src/com/company/Cfg.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Cfg {
+    private String N = "aSa";
+    private String E = "b" + N + "b";
+    private String S = "c";
+
+    @Override
+    public String toString() {
+        return "Cfg{" +
+                "N='" + N + '\'' +
+                ", E='" + E + '\'' +
+                ", S='" + S + '\'' +
+                '}';
+    }
+}
